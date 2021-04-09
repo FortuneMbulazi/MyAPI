fa71a3553c67a8b6aebf8c0aa1ff527df05d6b08
Simphiwe <simphiwem@designative.co.za>
2021-04-09 10:20:21 +0200

    my first API

---
+++ ConsoleDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading.Tasks;
+using ConsoleDemo;
+
+swaggerClient client = new("https://localhost:44380", new System.Net.Http.HttpClient());
+
+var records = await client.WeatherForecastAsync();
+
+foreach (var r in records)
+{
+    Console.WriteLine($"{r.Date}: {r.Summary}");
+}
